c8487d001fe05b3eed4f2c185eb58ddde5e831ce
Bhupinder Singh <88513851+bhupi1998@users.noreply.github.com>
2023-01-23 18:47:57 -0800

    All done. Added input verification and temp outputs
    scanf was reading a non clear input buffer

--- Lab2TempConversion/main.c
+++ Lab2TempConversion/main.c
@@ -14,22 +14,48 @@
  * 
  */
 int main() {
-    double userTemp; // user temperature entered by user
+    double userTemp, fTemp, cTemp, kTemp; // user temperature entered by user
     char tempScaleInput[5]; //stores user input
     char upperScaleInput[0]; //stores upper case converted input
+    char stdinCharacter[1024]; // used to purge stdin for scanf
+    //putting main program in loop so user can keep inputting
+    while(1){
     printf("Enter a temperature followed by F, C or K. e.g., 24.5F :");
     scanf("%lf%2s",&userTemp,tempScaleInput);
     upperScaleInput[0] = toupper(tempScaleInput[0]);
-    // if input does not have a valid ask again
-    while(upperScaleInput[0] != 'F' && upperScaleInput[0] != 'C' && upperScaleInput[0] != 'K'){
-        userTemp=0;
+    // if input is not valid, ask again
+    while(upperScaleInput[0] != 'F' && upperScaleInput[0] != 'C' && upperScaleInput[0] != 'K' && !isdigit(userTemp)){
+        fgets(stdinCharacter, 1024, stdin); //clears buffer so that scanf can wait for user input
         printf("\nInvalid input!");
-        printf("\nEnter a temperature followed by F, C or K. e.g., 24.5F :");
+        printf("\nEnter a temperature followed by F, C or K. e.g., 24.5F : ");
         scanf("%lf%2s",&userTemp,tempScaleInput);
         upperScaleInput[0] = toupper(tempScaleInput[0]);
     }
     printf("input string is: %0.1lf %s\n", userTemp, upperScaleInput);
     
+    // converting input temperature to the other scales
+    switch(upperScaleInput[0]){
+        case 'F':
+            fTemp=userTemp;
+            cTemp=(userTemp-32)*5/9;
+            kTemp=cTemp+273.15;
+            break;
+        case 'C':
+            cTemp=userTemp;
+            fTemp= cTemp*9/5+32;
+            kTemp= cTemp+273.15;
+            break;
+        case 'K':
+            kTemp=userTemp;
+            fTemp=(kTemp-273.15)*9/5+32;
+            cTemp=(fTemp-32)*5/9;
+            break;
+        default:
+            printf("something went wrong");
+    }
+    // output temps
+    printf("%0.3lfK = %0.3lfC = %0.3lfF\n",kTemp, cTemp, fTemp);
+    }
     return (EXIT_SUCCESS);
 }
 
