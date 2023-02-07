5487ca957d8f840986648c052b08edd11fed9e51
Bhupinder Singh <88513851+bhupi1998@users.noreply.github.com>
2023-02-07 09:41:28 -0800

    Dale's feedback
    -If 0 based language use <, not <=
    -Declare arrray size(Array[size] = {1,2,3})
      so that c prevents you from having too many things in the array. No savior for too few.

--- Lab4MakeChangePt2/main.c
+++ Lab4MakeChangePt2/main.c
@@ -16,9 +16,11 @@
 #include "makeChange.h"
 
 // I would like more explanation on this from dale
-const char *singleName[] = {"Twenty", "Ten", "Five", "Toonie", "Loonie", "Quarter", "Dime", "Nickel"};
-const char *pluralName [] = {"Twenties", "Tens", "Fives", "Toonies", "Loonies", "Quarters", "Dimes", "Nickels"};
-const double itemValue [] = {20,10,5,2,1,0.25,0.10,0.05};
+// Array size 8 is {0,1,2,3,4,5,6,7}
+// singleName is an array of pointers that points to the first character of each string which is at an address.
+const char *singleName[CURRENCY_DENOM] = {"Twenty", "Ten", "Five", "Toonie", "Loonie", "Quarter", "Dime", "Nickel"};
+const char *pluralName [CURRENCY_DENOM] = {"Twenties", "Tens", "Fives", "Toonies", "Loonies", "Quarters", "Dimes", "Nickels"};
+const double itemValue [CURRENCY_DENOM] = {20,10,5,2,1,0.25,0.10,0.05};
 /*
  * 
  */
@@ -33,7 +35,7 @@ int main() {
     change = (round(change*20))/20;
     printf("Rounded to the nearest nickel: %0.2lf\n", change);
     
-    for(int i=0; i<= CURRENCY_DENOM; i++){
+    for(int i=0; i< CURRENCY_DENOM; i++){
         change = changeItem (change, itemValue[i], singleName[i], pluralName[i]);
     }
     return (EXIT_SUCCESS);
--- Lab4MakeChangePt2/makeChange.h
+++ Lab4MakeChangePt2/makeChange.h
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   makeChange.h
  * Author: bhupi
@@ -14,18 +8,8 @@
 #ifndef MAKECHANGE_H
 #define MAKECHANGE_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-
-
-
-#ifdef __cplusplus
-}
-#endif
     #include <math.h>
-    #define CURRENCY_DENOM 7
+    #define CURRENCY_DENOM 8
     double changeItem (double change, const double itemValue, const char *singleName, const char *pluralName);
     double getPurchase (const char prompt[]);
     double getTender (const char prompt[], double purchasePrice);
