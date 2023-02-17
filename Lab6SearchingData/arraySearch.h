12a6facc0e9ac436ed39349105e15fa950382cbc
Bhupinder Singh <88513851+bhupi1998@users.noreply.github.com>
2023-02-16 17:45:30 -0800

    Made Lab 6

---
+++ Lab6SearchingData/arraySearch.h
@@ -0,0 +1,17 @@
+/* 
+ * File:   arraySearch.h
+ * Author: bhupi
+ *
+ * Created on February 16, 2023, 5:31 p.m.
+ */
+
+#ifndef ARRAYSEARCH_H
+#define ARRAYSEARCH_H
+
+/*
+ * linearSearch searches for value
+ */
+int linearSearch (const int value, const int numbers[], const int nNumbers);
+
+#endif /* ARRAYSEARCH_H */
+
---
+++ Lab6SearchingData/searchMain.c
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: bhupi
+ *
+ * Created on February 16, 2023, 5:29 p.m.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "arraySearch.h"
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
