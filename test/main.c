f45cafbe1628f5640a7ef1579ee72594693f7e5d
Bhupinder Singh <bhupi98.bs@gmail.com>
2023-03-24 20:43:24 -0700

    cleaning up

---
+++ test/main.c
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: bhupi
+ *
+ * Created on March 24, 2023, 8:12 p.m.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("hello bud");
+
+    return (EXIT_SUCCESS);
+}
+
