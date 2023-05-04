a5d23d0b5e27d2375dd4a6bba08ea916552a75ec
Bhupinder Singh <bhupi98.bs@gmail.com>
2023-05-03 19:16:48 -0700

    not sure

--- Lab6SearchingData/searchMain.c
+++ Lab6SearchingData/searchMain.c
@@ -20,49 +20,55 @@
  * 
  */
 int main(int argc, char** argv) {
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s numberFileName nNumbers\n", basename(argv[0]));
+    if(argc != 3){
+        fprintf(stderr,"Usage: %s numberFileName nNumbers\n", basename(argv[0]));
         return EXIT_FAILURE;
-    }
+    }    
     // pointer to requested binary file
     FILE *binaryFile;
     // ensuring binary file can be opened
-    if ((binaryFile = fopen(argv[1], "rb")) == NULL) {
-        fprintf(stderr, "%s: Unable to open %s because: %s\n", basename(argv[0]), argv[1], strerror(errno));
+    if( (binaryFile = fopen(argv[1],"rb")) == NULL){
+        fprintf(stderr,"%s: Unable to open %s because: %s\n", basename(argv[0]), argv[1], strerror(errno));
         return EXIT_FAILURE;
     }
     int fileIntegerSize;
-    if ((sscanf(argv[2], "%d", &fileIntegerSize)) != 1) {
-        fprintf(stderr, "%s: Unable to convert \"%s\" into an integer value.", basename(argv[0]), argv[2]);
+    if((sscanf(argv[2], "%d", &fileIntegerSize)) != 1){
+        fprintf(stderr,"%s: Unable to convert \"%s\" into an integer value.", basename(argv[0]), argv[2] );
         return EXIT_FAILURE;
     }
     // allocating memory to the heap
-    int *dataIntArray = (int*) malloc(fileIntegerSize * sizeof (int));
-    if ((fread(dataIntArray, sizeof (int), fileIntegerSize, binaryFile)) != fileIntegerSize) {
-        fprintf(stderr, "%s: Unable to read %d numbers from file \"%s\"\n", basename(argv[0]), fileIntegerSize, argv[1]);
+    int *dataIntArray = (int*)malloc(fileIntegerSize*sizeof(int));
+    if((fread(dataIntArray, sizeof(int), fileIntegerSize, binaryFile))!= fileIntegerSize){
+        fprintf(stderr,"%s: Unable to read %d numbers from file \"%s\"\n",basename(argv[0]), fileIntegerSize, argv[1]);
         return EXIT_FAILURE;
     }
     // closing file since everything has been saved in an array.
     fclose(binaryFile);
-
+    for(int i=0; i<100;i++){
+        printf("%d\n", dataIntArray[i]);
+    }
     int valToFind;
-    // see solution below
+    // ASK DALE:
+    //  12 characters is more than enough for any integer value
+    //  Issue is if more than 13 characters are made we'll start overflowing which causes
+    //      issues. Meets specified requirements but want to know better way.
     char userInput[13];
-    while (1) {
+    while(1){
         printf("Enter the integer value to find ('q' to quit) : ");
         scanf("%s", userInput);
-        if ((sscanf(userInput, "%d", &valToFind)) != 1) {
-            if ((userInput[0] == 'q' || userInput[0] == 'Q') && userInput[1] == '\0') {
-                printf("%s: Bye \n", basename(argv[0]));
-                return EXIT_SUCCESS;
-            } else {
+        if((sscanf(userInput, "%d", &valToFind)) != 1){
+            if((userInput[0] == 'q' || userInput[0] == 'Q') && userInput[1] == '\0'){
+            printf("%s: Bye \n", basename(argv[0]));
+            return EXIT_SUCCESS;
+            }
+            else{
                 printf("%s: Invalid Input\n", basename(argv[0]));
             }
-        } else {
-            int valIndex = linearSearch(valToFind, dataIntArray, fileIntegerSize);
-            if (valIndex == -1) {
+        }else{
+            int valIndex = linearSearch (valToFind, dataIntArray, fileIntegerSize);
+            if(valIndex == -1){
                 printf("%d was not found\n", valToFind);
-            } else {
+            }else {
                 printf("%d was found at position %d\n", valToFind, valIndex);
             }
         }
@@ -72,49 +78,3 @@ int main(int argc, char** argv) {
     return (EXIT_SUCCESS);
 }
 
-///*
-// * An example of lots of error handling when reading an integer
-// * value using strtol()
-// *
-// * (error messages should have gone to stderr).
-// *
-// * Dale Shpak, March 22, 2021
-// */
-//
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <errno.h>
-//#include <string.h>
-//#include <limits.h>
-//
-//int main() {
-//    printf("On this computer, a long is %d bytes and the maximum value is %ld\n", sizeof(long), LONG_MAX);
-//    while (1) {
-//	printf("Enter an integer value (that can be insanely huge): ");
-//	char s[101];
-//	scanf("%100s", s); // Read a maximum of 100 chars.  Safety mechanism for the array s.
-//
-//	char *endPtr;
-//	errno = 0; // Reset global error number
-//	long val = strtol(s, &endPtr, 10);
-//	int errCode = errno; // save the error number
-//	printf("Value read is: %ld", val);
-//	fflush(stdout);
-//	if (errCode == 0) {
-//	    printf(" which will fit into a long\n");
-//	} else{
-//	    printf(" but an error was detected: %s\n", strerror(errCode));
-//	}
-//
-//	if ((endPtr - s) < strlen(s)) {
-//	    printf(" Stopped after reading %ld characters\n", (endPtr - s));
-//	}
-//	// Check if this long value will fit into an int
-//	if (val > INT_MAX || val < INT_MIN) {
-//	    printf("%s won't fit into a regular int\n", s);
-//	} else {
-//	    printf("%s will fit into a regular int\n", s);
-//	}
-//    }
-//    return EXIT_SUCCESS;
-//}
